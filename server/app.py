2a1ccb81f2813aa2f1772a191913b3e74f3a51c3
mxriuki <mwendav37@gmail.com>
2024-09-30 12:28:48 +0300

    done

--- server/app.py
+++ server/app.py
@@ -3,3 +3,15 @@
 from flask import Flask
 
 app = Flask(__name__)
+
+# @aoo.route decorator registers the index() function with the Flask application instance app
+@app.route('/')
+def index():
+    return '<h1>Welcome to my page!</h1>'
+
+@app.route('/<string:username>')
+def user(username):
+    return f'<h1>Profile for {username}</h1>'
+
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
